f194f9cac91f38aca27b0fe0a1e272fc5ffe6039
Vhaefeli <vhaefeli@student.42lausanne.ch>
2023-06-23 18:10:15 +0200

    ex01 ok

---
+++ ex01/RPN.hpp
@@ -0,0 +1,18 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#define RED "\033[31;1m"
+#define GRE "\033[32;1m"
+#define YEL "\033[33;1m"
+#define BLU "\033[34;1m"
+#define NOC "\033[0m"
+
+#include <string>
+#include <stack>
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+
+double RPNcalculate(std::string arg);
+
+#endif
